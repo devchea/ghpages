62ffb969a484bd9d57d85ffc254bc45b0daaffd7
devchea <rchea817@gmail.com>
2020-11-03 21:52:23 -0800

    "updates"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react"
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -19,9 +19,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
         <a
           className="App-link"
